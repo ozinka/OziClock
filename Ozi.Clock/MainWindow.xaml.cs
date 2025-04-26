210f2e4f67b80c1fcc03074e68764c4086261e17
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-04-26 13:13:18 +0200

    update usings

--- Ozi.Clock/MainWindow.xaml.cs
+++ Ozi.Clock/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
